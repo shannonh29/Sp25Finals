4f7c6b25cdd39d5e5aeb10c7f15e179c69c0d30e
shannonh29 <72118543+shannonh29@users.noreply.github.com>
2025-05-04 19:09:46 -0500

    added schedule and updated CS 357 grade table

--- src/pages/CS357.js
+++ src/pages/CS357.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CS357 = () => {
+
+    // data table containing general information for grades
     const data = [
-        {name: "Bi-Weekly Quizzes", contribution: 35, status: "Completed"},
-        {name: "Homework", contribution: 25, status: "Completed"},
-        {name: "MPs", contribution: 10, status: "Completed"},
-        {name: "Group Activities", contribution: 11, status: "Completed"},
-        {name: "Pre-Lecture Activities", contribution: 4, status: "Completed"},
-        {name: "Final Exam", contribution: 15, status: "Not Completed"}
+        {name: "Bi-Weekly Quizzes", contribution: 35, amt_earned: null, status: "Completed"},
+        {name: "Homework", contribution: 25, amt_earned: null, status: "Completed"},
+        {name: "MPs", contribution: 10, amt_earned: null, status: "Completed"},
+        {name: "Group Activities", contribution: 11, amt_earned: null, status: "Completed"},
+        {name: "Pre-Lecture Activities", contribution: 4, amt_earned: null, status: "Completed"},
+        {name: "Final Exam", contribution: 15, amt_earned: null, status: "Not Completed"},
+        {name: "Total", contribution: null, amt_earned: null, status: "Not Completed"}
     ];
 
+    // average grade for each topic
+    const avg = [
+        {name: "Bi-Weekly Quizzes", avg: 0.9188114800000001},
+        {name: "Homework", avg: 1.00},
+        {name: "MPs", avg: 0.992},
+        {name: "Group Activities", avg: 1.0194047616666666},
+        {name: "Pre-Lecture Activities", avg: 1.00},
+        {name: "Final Exam", avg: null}
+    ];
+
+    // calculating and inputting the amount earned based on contribution and average
+    data.forEach(item => {
+        const avg_item = avg.find(a => a.name === item.name);
+        if(avg_item) {
+            item.amt_earned = (avg_item != null ? avg_item.avg * item.contribution : 0);
+        }
+    });
+
+    // calculating the total contribution and amount earned
+    const total_item = data.find(a => a.name === "Total");
+    total_item.contribution = data.reduce((sum, item) => 
+        item.name !== "Total" ? sum + (item.contribution || 0) : sum, 0);
+    total_item.amt_earned = data.reduce((sum, item) =>
+        item.name !== "Total" ? sum + (item.amt_earned || 0) : sum, 0);
+
+    // creating the table
     return (
         <div className="container mt-3"> 
             <table className="table">
                 <thead className="table-light">
                     <tr>
                         <th>Name</th>
-                        <th>Contribution</th>
+                        <th>Contribution (%)</th>
+                        <th>Amount Earned (%)</th>
                         <th>Status</th>
                     </tr>
                 </thead>
@@ -26,6 +56,7 @@ const CS357 = () => {
                         <tr key={data_pt.name}>
                             <td>{data_pt.name}</td>
                             <td>{data_pt.contribution}</td>
+                            <td>{data_pt.amt_earned}</td>
                             <td>
                                 <span className={`badge ${
                                     data_pt.status === "Completed" ? "bg-success" :
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React from 'react';
+import './schedule.css';
 
 const Schedule = () => {
+    const week1 = ['Monday 5/5', 'Tuesday 5/6', 'Wednesday 5/7', 'Thursday 5/8', 'Friday 5/9', 'Saturday 5/10', 'Sunday 5/11'];
+
     return (
-        <div>
-            <h1>
-                This is the Schedule
-            </h1>
+        <div class="page">
+            <h1 class="title">Schedule</h1>
+            <div class="boxes_container">
+                <div class="box">Monday 5/5</div>
+                <div class="box">Tuesday 5/6</div>
+                <div class="box">Wednesday 5/7</div>
+                <div class="box">Thursday 5/8</div>
+                <div class="box">Friday 5/9</div>
+                <div class="box">Saturday 5/10</div>
+                <div class="box">Sunday 5/11</div>
+            </div>
         </div>
-    )
-}
+
+    );
+} 
+
 
 export default Schedule;
